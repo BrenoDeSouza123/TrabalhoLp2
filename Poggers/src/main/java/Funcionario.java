1ebde44ac784c56d5026f4a66c846ff322b80a75
Octtane <165536174+Octtane@users.noreply.github.com>
2025-03-26 14:18:36 -0300

    Add files via upload

---
+++ Poggers/src/main/java/Funcionario.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Kayky Bittencourt
+ */
+public class Funcionario {
+    
+    private String nome;
+    private String CNPJ;
+    private String telefone;
+    private String email;
+    private String endereco;
+    
+    Funcionario(String nome, String CNPJ, String telefone, String email, String endereco){
+    this.nome = nome;
+    this.CNPJ = CNPJ;
+    this.telefone = telefone;
+    this.email = email;
+    this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    
+    
+}
