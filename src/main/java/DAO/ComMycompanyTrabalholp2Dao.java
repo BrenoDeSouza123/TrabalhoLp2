7303ab1373ecc94364b6cc14d6cfd9ba6ff9df96
Beatriz Freitas <beatrizfreitas314.bf@gmail.com>
2025-04-12 14:08:58 -0300

    atualizado

---
+++ src/main/java/DAO/ComMycompanyTrabalholp2Dao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.com.mycompany.trabalholp2.dao;
+
+/**
+ *
+ * @author manoel
+ */
+public class ComMycompanyTrabalholp2Dao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/trabalholp2/TrabalhoLp2.java
@@ -0,0 +1,8 @@
+package com.mycompany.trabalholp2;
+
+public class TrabalhoLp2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
