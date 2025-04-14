351105efe7905e46146a15d37b3fd43099833bc1
BrenoDeSouza123 <148013716+BrenoDeSouza123@users.noreply.github.com>
2025-04-14 20:08:24 -0300

    Add files via upload

---
+++ TrabalhoLp2-full_Project/src/main/java/com/mycompany/trabalholp2/Classes/Produto.java
@@ -0,0 +1,63 @@
+package com.mycompany.trabalholp2.Classes;
+/*
+  @author Daniel Souza
+*/
+public class Produto {
+    private String nome_prod;
+    private int qtde;
+    private String categoria;
+    private String fornecedor;
+    private Integer Id;
+    
+    public Produto (){
+        
+    }
+
+    public Produto(String nome_prod, int qtde,  String categoria, String fornecedor) {
+        this.nome_prod = nome_prod;
+        this.qtde = qtde;
+        this.categoria = categoria;        
+        this.fornecedor = fornecedor;
+    }
+    public String getNome_prod(){
+        return nome_prod;
+    }
+    public void setNome_prod(String nome_prod){
+        this.nome_prod = nome_prod;
+    }
+    public String getCategoria(){
+        return categoria;
+    }
+    public void setCategoria(String categoria){
+        this.categoria = categoria;
+    }
+    public int getQtde(){
+        return qtde;
+    }
+    public void setQtde(int qtde){
+        this.qtde = qtde;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer Id) {
+        this.Id = Id;
+    }
+
+    /**
+     * @return the fornecedor
+     */
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    /**
+     * @param fornecedor the fornecedor to set
+     */
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+        
+}
